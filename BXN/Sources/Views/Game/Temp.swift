f8e052112bfaa9afdd0e448ee7283e964956d0ce
we-z <yousefsalem00@gmail.com>
2024-12-03 12:30:27 -0800

    new icon

--- BXN/Sources/Views/Game/Temp.swift
+++ BXN/Sources/Views/Game/Temp.swift
@@ -11,19 +11,23 @@ struct Temp: View {
     var body: some View {
         ZStack {
             Color.black
-//                .opacity(0.95)
-            HStack{
+            Color.white
+                .opacity(0.06)
+            LinearGradient(gradient: Gradient(colors: [.gray.opacity(0.2), .clear]), startPoint: UnitPoint(x: 0.5, y: 0), endPoint: UnitPoint(x: 0.5, y: 1))
+                .aspectRatio(contentMode: .fit)
+//            HStack{
                 Text("B")
                     .foregroundColor(.white)
-                    .font(.system(size: 150))
-                Text("x")
+                    .font(.system(size: 240))
+//                Text("x")
+//                    .foregroundColor(.white)
+//                    .font(.system(size: 120))
+//                    .offset(y: -12)
+                Text("n")
                     .foregroundColor(.white)
                     .font(.system(size: 120))
-                    .offset(y: -12)
-                Text("N")
-                    .foregroundColor(.white)
-                    .font(.system(size: 150))
-            }
+                    .offset(x: 90, y: -110)
+//            }
         }
     }
 }
