6c44d810cb0b3337eb1b024041b2d1b6de231e44
we-z <yousefsalem00@gmail.com>
2024-12-05 15:13:49 -0800

    white icon

--- BXN/Sources/Views/Game/Temp.swift
+++ BXN/Sources/Views/Game/Temp.swift
@@ -13,18 +13,18 @@ struct Temp: View {
             Color.black
             Color.white
                 .opacity(0.06)
-            LinearGradient(gradient: Gradient(colors: [.gray.opacity(0.3), .clear]), startPoint: UnitPoint(x: 0.5, y: 0), endPoint: UnitPoint(x: 0.5, y: 1))
+            LinearGradient(gradient: Gradient(colors: [.white, .white]), startPoint: UnitPoint(x: 0.5, y: 0), endPoint: UnitPoint(x: 0.5, y: 1))
                 .aspectRatio(contentMode: .fit)
 //            HStack{
                 Text("B")
-                    .foregroundColor(.white)
+                .foregroundColor(.black)
                     .font(.system(size: 240))
 //                Text("x")
 //                    .foregroundColor(.white)
 //                    .font(.system(size: 120))
 //                    .offset(y: -12)
                 Text("n")
-                    .foregroundColor(.white)
+                .foregroundColor(.black)
                     .font(.system(size: 120))
                     .offset(x: 90, y: -110)
 //            }
